25d18f9ceb6d5eae1097b0011055c318169cb3f6
chiao <php@html.js.cn>
2024-06-13 17:14:10 +0800

    new page everytime

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const puppeteer = require('puppeteer-core');
 const app = express();
 
 let browser = null;
-let page = null;
 
 async function init() {
   if (!browser || !browser.connected) {
@@ -19,40 +18,51 @@ async function init() {
       });
     }
   }
-  await initPage();
 }
 
 async function initPage() {
-  await browser.pages().then(r => {
-    if (r.length) page = r[0];
-  })
-  if (!page || page.isClosed() || page.mainFrame().isDetached()) {
-    console.log("init page");
-    page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on('request', ir => {
-      if (ir.isInterceptResolutionHandled()) return;
-      let url = ir.url();
-      if (
-        url.includes('.png') ||
-        url.includes('.jpg') ||
-        url.includes('.gif') ||
-        url.includes('.do?')
-      )
-        ir.abort();
-      else ir.continue();
-    });
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on('request', ir => {
+    if (ir.isInterceptResolutionHandled()) return;
+    let url = ir.url();
+    if (
+      url.includes('.png') ||
+      url.includes('.jpg') ||
+      url.includes('.gif') ||
+      url.includes('.do?')
+    )
+      ir.abort();
+    else ir.continue();
+  });
+  return page;
+}
+
+function closePage(page)
+{
+  try {
+    if (page) page.close();
+  } catch (e) {
   }
 }
 
 async function query(url) {
+  await init();
+  const page = await initPage();
   const queryPromise = new Promise (resolve => {
     page.on('response', async response => {
       if (response.url().includes('query?type=')) {
         page.removeAllListeners('response');
         resolve(await response.json());
+        setTimeout(() => {
+          closePage(page);
+        }, 5000);
       }
     });
+    setTimeout(async () => {
+      resolve({timeout: true});
+      closePage(page);
+    }, 20000);
   });
   page.goto(url);
   return queryPromise;
@@ -60,7 +70,6 @@ async function query(url) {
 
 app.get('/query', async (req, res) => {
   try {
-    await init();
     return res.json(await query('https://www.kuaidi100.com/all/' + req.query.com + '.shtml?nu=' + req.query.nu));
   } catch (e) {
     console.error(e);
